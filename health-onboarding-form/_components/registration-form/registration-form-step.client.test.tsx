1f056704c4d9f1ddec81c42e5d4c24939852e4b8
Martin Luo <aluo1.developer@gmail.com>
2026-02-16 15:35:59 +0800

    feat: define registration step component

---
+++ health-onboarding-form/_components/registration-form/registration-form-step.client.test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import { RegistrationFormStep } from "./registration-form-step";
+
+describe("RegistrationFormStep", () => {
+  it("renders the correct number of steps", () => {
+    const { getAllByRole } = render(<RegistrationFormStep currentStep={0} />);
+    const steps = getAllByRole("listitem");
+    expect(steps).toHaveLength(4);
+  });
+});
---
+++ health-onboarding-form/_components/registration-form/registration-form-step.tsx
@@ -0,0 +1,36 @@
+import { Step, StepLabel, Stepper } from "@mui/material";
+import React from "react";
+
+export enum RegistrationStep {
+  PersonalInfo = "personal-info",
+  HealthInfo = "health-info",
+  Membership = "membership",
+  Review = "review",
+}
+
+export const RegistrationStepLabels = {
+  [RegistrationStep.PersonalInfo]: "Personal Info",
+  [RegistrationStep.HealthInfo]: "Health Info",
+  [RegistrationStep.Membership]: "Membership",
+  [RegistrationStep.Review]: "Review",
+};
+
+const steps = Object.values(RegistrationStepLabels);
+
+type RegistrationFormStepProps = {
+  currentStep: number;
+};
+
+export const RegistrationFormStep = ({
+  currentStep,
+}: RegistrationFormStepProps) => {
+  return (
+    <Stepper activeStep={currentStep} alternativeLabel component="ol">
+      {steps.map((label) => (
+        <Step key={label} component="li">
+          <StepLabel>{label}</StepLabel>
+        </Step>
+      ))}
+    </Stepper>
+  );
+};
